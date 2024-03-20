a9d705730caf3d17ceda576eb5f932662e814089
Samuel <mucabatera@gmail.com>
2024-03-20 17:42:20 -0300

    REFS #busca por Samuel Oliveira: Acertando o FinalizaPagamento

--- app/controllers/loja_controller.rb
+++ app/controllers/loja_controller.rb
@@ -3,8 +3,8 @@ include HTTParty
   before_action :set_veiculo, except: [ :index, :pagamento ] 
   before_action :set_cliente_cookie
   after_action :capturar_destino
-  before_action :authenticate_user!, only: %i[ aluguel locacao ]
-  before_action :set_token_pagamento, only: %i[ aluguel locacao ]
+  before_action :authenticate_user!, only: %i[ aluguel locacao pagamento ]
+  before_action :set_token_pagamento, only: %i[ aluguel locacao pagamento ]
   before_action :set_reserva, only: %i[ pagamento ]
 
   def index
